1f5f0f9c75dc3e584724f830de5133cd4c570462
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-04-13 15:53:40 -0500

    added new actors

---
+++ src/main/scala/akkastudy/simpleactor/scala/SimpleActorScala.scala
@@ -0,0 +1,13 @@
+package akkastudy.simpleactor.scala
+
+import akka.actor.Actor
+import akka.event.Logging
+
+class SimpleActorScala extends Actor {
+   val log = Logging(context.system, this)
+
+   def receive = {
+     case "test" ⇒ log.info("received message test in Simple Actor Scala")
+     case _ ⇒ log.info("received unknown message test in Simple Actor Scala")
+   }
+ }
---
+++ src/main/scala/akkastudy/stopactor/scala/StopActorScala.scala
@@ -0,0 +1,17 @@
+package akkastudy.stopactor.scala
+
+import akka.actor.Actor
+import akka.event.Logging
+
+class StopActorScala extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case "test" ⇒ log.info("received message test in Simple Actor Scala")
+    case _ ⇒ log.info("received unknown message test in Simple Actor Scala")
+  }
+
+  override def postStop() {
+    println("Actor stopped")
+  }
+}
