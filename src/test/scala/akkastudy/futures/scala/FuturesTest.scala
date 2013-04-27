f804124cbd761e8508637f2951e6817e860b079c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-04-27 14:53:03 -0600

    Uncommented FuturesTest.scala to include all test

--- src/test/scala/akkastudy/futures/scala/FuturesTest.scala
+++ src/test/scala/akkastudy/futures/scala/FuturesTest.scala
@@ -10,7 +10,6 @@ import scala.concurrent.Future
 import akka.util.Timeout
 import scala.concurrent.duration._
 import concurrent.Await
-import akkastudy.SenderActor
 
 
 /**
@@ -31,54 +30,52 @@ import akkastudy.SenderActor
  * akkastudy.futures.scala.FuturesTest are a suite of tests that
  * show various uses of Futures in Scala.
  */
-
 class FuturesTest extends FlatSpec with MustMatchers {
   behavior of "An Actor System should"
 
-//  it should "A future is a object that has an answer but not quite yet delivered" in {
-//    //An execution context is required
-//    implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(12))
-//
-//    val future = Future {
-//      "Hello" + " " + "World"
-//    }
-//
-//    implicit val timeout = Timeout(5 seconds)
-//    println("Step 1")
-//    val result = Await.result(future, timeout.duration) //blocking
-//    println("Step 2: " + result)
-//
-//    result must equal("Hello World")
-//  }
-//
-//  it should "A future is a object that has an answer but not quite yet delivered, and can be read non-blocking" in {
-//    //An execution context is required
-//    implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(12))
-//
-//    val future = Future {
-//      "Hello" + " " + "World"
-//    }
-//
-//    future foreach (x => println("I got an answer" + x)) //asynchonous
-//    println("running1")
-//    println("running2")
-//    println("running3")
-//
-//  }
+  it should "A future is a object that has an answer but not quite yet delivered" in {
+    //An execution context is required
+    implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(12))
 
-//  it should "be able to ask information of an Actor, where as the the Actor will return the answer, blocked" in {
-//    import akka.pattern.ask
-//    import system.dispatcher
-//
-//    implicit val timeout = util.Timeout(1 seconds)
-//
-//    val system = ActorSystem("MySystem")
-//    val futureActor = system.actorOf(Props[FuturesActor], "futuresActor")
-//    val promise = (futureActor ? "What is the meaning of love?")
-//    val result = Await.result(promise, timeout.duration) //Blocking
-//    println(result)
-//    Thread.sleep(10000)
-//  }
+    val future = Future {
+      "Hello" + " " + "World"
+    }
+
+    implicit val timeout = Timeout(5 seconds)
+    println("Step 1")
+    val result = Await.result(future, timeout.duration) //blocking
+    println("Step 2: " + result)
+
+    result must equal("Hello World")
+  }
+
+  it should "A future is a object that has an answer but not quite yet delivered, and can be read non-blocking" in {
+    //An execution context is required
+    implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(12))
+
+    val future = Future {
+      "Hello" + " " + "World"
+    }
+
+    future foreach (x => println("I got an answer" + x)) //asynchonous
+    println("running1")
+    println("running2")
+    println("running3")
+
+  }
+
+  it should "be able to ask information of an Actor, where as the the Actor will return the answer, blocked" in {
+    import akka.pattern.ask
+
+    implicit val timeout = util.Timeout(1 seconds)
+
+    val system = ActorSystem("MySystem")
+    val futureActor = system.actorOf(Props[FuturesActor], "futuresActor")
+    val promise = (futureActor ? "What is the meaning of love?")
+    val result = Await.result(promise, timeout.duration) //Blocking
+    println(result)
+    Thread.sleep(10000)
+  }
 
   it should "A future is a object that has an answer but not quite yet delivered" in {
     //An execution context is required
@@ -112,32 +109,31 @@ class FuturesTest extends FlatSpec with MustMatchers {
   }
 
   it should "be able to ask information of an Actor, " +
-            "where as the the Actor will return the answer, blocked" in {
+    "where as the the Actor will return the answer, blocked" in {
     import akka.pattern.ask
-    import system.dispatcher
 
     implicit val timeout = util.Timeout(60 seconds)
 
     val system = ActorSystem("MySystem")
     val futureActor = system.actorOf(Props[FuturesActor], "futuresActor")
     val promise = (futureActor ? "What is the meaning of love?")
-    val result = Await.result(promise, timeout.duration)    //blocked
+    val result = Await.result(promise, timeout.duration) //blocked
     println(result)
     Thread.sleep(5000)
   }
 
   it should "be able to ask information of an Actor, where as the the Actor will return the answer, non blocked" in {
-      import akka.pattern.ask
-      import system.dispatcher
+    import akka.pattern.ask
+    import system.dispatcher
 
-      implicit val timeout = util.Timeout(10 seconds)
+    implicit val timeout = util.Timeout(10 seconds)
 
-      val system = ActorSystem("MySystem")
-      val futureActor = system.actorOf(Props[FuturesActor], "futuresActor")
-      val promise = (futureActor ? "What is the meaning of love?")
-      val promise2 = (futureActor ? "What is the meaning of beef and cheddar?")
-      promise2 foreach (x=> println("promise 2" + x))  //non blocking
-      promise foreach (x=> println("promise" + x))  //non blocking
-      Thread.sleep(5000)
+    val system = ActorSystem("MySystem")
+    val futureActor = system.actorOf(Props[FuturesActor], "futuresActor")
+    val promise = (futureActor ? "What is the meaning of love?")
+    val promise2 = (futureActor ? "What is the meaning of beef and cheddar?")
+    promise2 foreach (x => println("promise 2" + x)) //non blocking
+    promise foreach (x => println("promise" + x)) //non blocking
+    Thread.sleep(5000)
   }
 }
