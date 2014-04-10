7582d26fe70ab921004a9687c0cfde47961f2baf
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-04-10 12:07:28 -0600

    Renamed DeadLetterListener to ListenerActor

--- src/main/scala/akkastudy/eventstream/scala/ListenerActor.scala
+++ src/main/scala/akkastudy/eventstream/scala/ListenerActor.scala
@@ -3,11 +3,11 @@ package akkastudy.eventstream.scala
 import akka.actor.{DeadLetter, Actor}
 import akka.event.Logging
 
-class DeadLetterListenerActor extends Actor {
+class ListenerActor extends Actor {
   val log = Logging(context.system, this)
 
   def receive = {
     case x:DeadLetter ⇒ log.info("received a dead letter with message: {}", x.message)
-    case _ ⇒ log.info("received unknown message test in Simple Actor Scala")
+    case y ⇒ log.info("received unknown message test in Simple Actor Scala: {}", y)
   }
 }
