4ab405e0a2784a8c33b1381f451580f174503f86
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-04-10 12:08:25 -0600

    Added another test in EventStreamTest

--- src/test/scala/akkastudy/eventstream/scala/EventStreamTest.scala
+++ src/test/scala/akkastudy/eventstream/scala/EventStreamTest.scala
@@ -2,15 +2,24 @@ package akkastudy.eventstream.scala
 
 import akka.actor.{DeadLetter, Props, ActorSystem}
 import org.scalatest.FlatSpec
+import akkastudy.eventbus.scala.SimpleActor
 
 class EventStreamTest extends FlatSpec {
   behavior of "An Actor System should"
 
   it should "throw send a dead letter if the actor is not found" in {
     val system = ActorSystem("MySystem")
-    val deadLetterActor = system.actorOf(Props(classOf[DeadLetterListenerActor]), "deadLetterListenerActor")
-    system.eventStream.subscribe(deadLetterActor, classOf[DeadLetter])
+    val deadLetterListenerActor = system.actorOf(Props(classOf[ListenerActor]), "deadLetterListenerActor")
+    system.eventStream.subscribe(deadLetterListenerActor, classOf[DeadLetter])
     val myActor = system.actorSelection("akka://MySystem/user/somethingElseIShouldn\'tBeLookingFor")
     myActor ! "Simple Test"
+  }
+
+  it should "throw send refer to an actor when an event happens" in {
+    val system = ActorSystem("MySystem")
+    val deadLetterListenerActor = system.actorOf(Props(classOf[ListenerActor]), "deadLetterListenerActor")
+    val simpleActor = system.actorOf(Props(classOf[SimpleActor]), "simpleActor")
+    system.eventStream.subscribe(deadLetterListenerActor, classOf[String])
+    simpleActor ! "Simple Test"
   }
 }
