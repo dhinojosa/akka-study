9dde64a25c1b6324dbd9214813b451d100645e1d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-04-19 21:42:14 -0600

    direct remote actor

--- src/test/scala/akkastudy/remote/scala/RemoteSimpleActorTest.scala
+++ src/test/scala/akkastudy/remote/scala/RemoteSimpleActorTest.scala
@@ -24,19 +24,34 @@ class RemoteSimpleActorTest extends FlatSpec {
     actorSystemBeta.awaitTermination()
   }
 
-//  it should "receive our message in Scala but remotely with different setup" in {
-//    val config = ConfigFactory.load()
-//    val actorSystemBeta = ActorSystem("actorSystemBeta", config.getConfig("remote-system-beta").withFallback(config))
-//    val actorSystemAlpha  = ActorSystem("actorSystemAlpha", config.getConfig("remote-system-alpha").withFallback(config))
-//
-//    val senderActor = actorSystemAlpha.actorOf(Props[SenderActorScala], name="senderActorScala")
-//
-//    senderActor ! "Do it"
-//
-//    Thread.sleep(5000)
-//    actorSystemAlpha.shutdown()
-//    actorSystemAlpha.awaitTermination()
-//    actorSystemBeta.shutdown()
-//    actorSystemBeta.awaitTermination()
-//  }
+  it should "receive our message in Scala but remotely with different setup" in {
+    val config = ConfigFactory.load()
+    val actorSystemBeta = ActorSystem("actorSystemBeta", config.getConfig("remote-system-beta").withFallback(config))
+    val actorSystemAlpha  = ActorSystem("actorSystemAlpha", config.getConfig("remote-system-alpha").withFallback(config))
+
+    val senderActor = actorSystemAlpha.actorOf(Props[SenderActorScala], name="senderActorScala")
+
+    senderActor ! "Do it"
+
+    Thread.sleep(5000)
+    actorSystemAlpha.shutdown()
+    actorSystemAlpha.awaitTermination()
+    actorSystemBeta.shutdown()
+    actorSystemBeta.awaitTermination()
+  }
+
+  it should "be able to call a remote actor via URL" in {
+    val config = ConfigFactory.load()
+    val remoteActorSystem  = ActorSystem("remoteActorSystem", config.getConfig("remote-system").withFallback(config))
+
+    val senderActor = remoteActorSystem.actorOf(Props[SimpleActorScala], name="simpleActorScala")
+
+    senderActor ! "Do it"
+
+    remoteActorSystem.actorSelection("akka.tcp://remoteActorSystem:10190/user/simpleActorScala")
+
+    Thread.sleep(5000)
+    remoteActorSystem.shutdown()
+    remoteActorSystem.awaitTermination()
+  }
 }
