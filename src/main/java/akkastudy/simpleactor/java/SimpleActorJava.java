aa3160d7ab2beb6cbf65ca79248cfdbc2e5dc7ec
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-05 14:17:31 -0700

    Corrected formatting so that it can fit on a screen

--- src/main/java/akkastudy/simpleactor/java/SimpleActorJava.java
+++ src/main/java/akkastudy/simpleactor/java/SimpleActorJava.java
@@ -5,11 +5,13 @@ import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
 public class SimpleActorJava extends UntypedActor {
-    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    LoggingAdapter log =
+            Logging.getLogger(getContext().system(), this);
 
     public void onReceive(Object message) {
         if (message instanceof String)
-            log.info("Received String message in SimpleActorJava {}", message);
+            log.info("Received String message " +
+                    "in SimpleActorJava {}", message);
         else
             unhandled(message);
     }
