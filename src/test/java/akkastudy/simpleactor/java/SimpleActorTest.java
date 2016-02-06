5af6c527217d22f3841f2ba1aada153485a36ccc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 14:42:33 -0700

    Using new termination

--- src/test/java/akkastudy/simpleactor/java/SimpleActorTest.java
+++ src/test/java/akkastudy/simpleactor/java/SimpleActorTest.java
@@ -6,21 +6,28 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.japi.Creator;
 import org.junit.Test;
+import scala.concurrent.Await;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 public class SimpleActorTest {
 
     @Test
-    public void testActor() {
+    public void testActor() throws Exception {
         ActorSystem system = ActorSystem.create("MySystem");
         ActorRef myActor = system.actorOf(
                 Props.create(SimpleActorJava.class),
                 "simpleActorJava");
-        ActorSelection deadLettersActor = system.actorSelection("/deadLetters");
-        myActor.tell("Bueno!", deadLettersActor.anchor());
+
+        System.out.println(Thread.currentThread().getName());
+        myActor.tell("Bueno!", system.deadLetters());
+        Thread.sleep(10000);
+        Await.result(system.terminate(), Duration.apply(10, TimeUnit.SECONDS));
     }
 
     @Test
-    public void testActorWithAFactory() {
+    public void testActorWithAFactory() throws Exception {
         ActorSystem system = ActorSystem.create("MySystem");
         ActorRef myActor = system.actorOf(
                 Props.create(SimpleActorJava.class, new Creator<SimpleActorJava>() {
@@ -30,7 +37,26 @@ public class SimpleActorTest {
                     }
                 }), "simpleActorJava"
         );
-        ActorSelection deadLettersActorSelection = system.actorSelection("/deadLetters");
-        myActor.tell("Bueno!", deadLettersActorSelection.anchor());
+        myActor.tell("Bueno!", system.deadLetters());
+        Thread.sleep(10000);
+        Await.result(system.terminate(), Duration.apply(10, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void testActorSelection() throws Exception {
+        ActorSystem system = ActorSystem.create("MySystem");
+        system.actorOf(
+                Props.create(SimpleActorJava.class),
+                "simpleActorJava");
+
+        Thread.sleep(2000);
+
+        System.out.println(Thread.currentThread().getName());
+
+        ActorSelection myActor = system.actorSelection("akka://MySystem/user/simpleActorJava");
+
+        myActor.tell("Bueno!", system.deadLetters());
+        Thread.sleep(10000);
+        Await.result(system.terminate(), Duration.apply(10, TimeUnit.SECONDS));
     }
 }
