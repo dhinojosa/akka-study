127cf01342ab905f2f0cae5d44f5f684d98e292f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 14:13:07 -0700

    Using new termination

--- src/test/scala/akkastudy/simpleactor/scala/SimpleActorTest.scala
+++ src/test/scala/akkastudy/simpleactor/scala/SimpleActorTest.scala
@@ -4,18 +4,21 @@ import org.scalatest.FlatSpec
 import akka.actor.{Props, ActorSystem}
 
 class SimpleActorTest extends FlatSpec {
+
   behavior of "A simple actor"
 
   it should "receive our message in Scala" in {
     val system = ActorSystem("MySystem")
-    val myActor = system.actorOf(Props[SimpleActorScala], name = "simpleActorScala")
+    val myActor = system.actorOf(Props[SimpleActorScala],
+                  name = "simpleActorScala")
     myActor ! "Simple Test"
     myActor ! "test"
   }
 
   it should "receive our message in Scala using a factory" in {
     val system = ActorSystem("MySystem")
-    val myActor = system.actorOf(Props(new SimpleActorScala), name = "simpleActorScala")
+    val myActor = system.actorOf(Props(new SimpleActorScala),
+      name = "simpleActorScala")
     myActor ! "Simple Test"
     myActor ! "test"
   }
@@ -23,7 +26,10 @@ class SimpleActorTest extends FlatSpec {
   it should "be at location akka://MySystem/user/simpleActorJava" in {
     val system = ActorSystem("MySystem")
     system.actorOf(Props[SimpleActorScala], name = "simpleActorJava")
+
+
     val actorSelection = system.actorSelection("akka://MySystem/user/simpleActorJava")
+    println("Starting location selection")
     actorSelection ! "Simple Test"
   }
 
