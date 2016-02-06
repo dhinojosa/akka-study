536b4f1a40bb8781ba2d7948b9d804fef903a9c4
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 15:28:49 -0700

    Changed Formatting of AskActor added a Thread.sleep for presentation

--- src/main/scala/akkastudy/askactor/scala/AskActor.scala
+++ src/main/scala/akkastudy/askactor/scala/AskActor.scala
@@ -7,7 +7,11 @@ class AskActor extends Actor {
   val log = Logging(context.system, this)
 
   def receive = {
-    case "Ping" ⇒ sender ! "Pong"
+    case "Ping" ⇒ {
+      log.info("Recieved your ping, coming back at ya!")
+      Thread.sleep(3000)
+      sender ! "Pong"
+    }
     case _ ⇒ log.info("received unknown message test in AskActor")
   }
 }
