148fbe1f12836e44bb4e29517210d66488eeb2c8
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 14:14:25 -0700

    Using new termination

--- src/test/scala/akkastudy/router/scala/RouterTest.scala
+++ src/test/scala/akkastudy/router/scala/RouterTest.scala
@@ -1,5 +1,7 @@
 package akkastudy.router.scala
 
+import java.util.concurrent.TimeUnit
+
 import org.scalatest.{Matchers, FlatSpec}
 import akka.actor.{Props, ActorSystem}
 import akka.routing.FromConfig
@@ -7,8 +9,11 @@ import akkastudy.SenderActor
 import com.typesafe.config.ConfigFactory
 import akkastudy.simpleactor.scala.SimpleActorScala
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 class RouterTest extends FlatSpec with Matchers {
-  behavior of "An Router"
+  behavior of "a Router"
 
   it should
     """hand off work to each router to distribute load. It
@@ -18,8 +23,8 @@ class RouterTest extends FlatSpec with Matchers {
         config.getConfig("routing-system").withFallback(config))
     val router = system.
       actorOf(Props[SimpleActorScala].withRouter(FromConfig()), "simplerouter")
-
     val senderActor = system.actorOf(Props[SenderActor])
+
     (router ! "test")(senderActor)
     (router ! "test")(senderActor)
     (router ! "test")(senderActor)
@@ -29,5 +34,8 @@ class RouterTest extends FlatSpec with Matchers {
     (router ! "test")(senderActor)
     (router ! "test")(senderActor)
     (router ! "test")(senderActor)
+
+    Thread.sleep(15000)
+    Await.result(system.terminate(), Duration(10, TimeUnit.SECONDS))
   }
 }
