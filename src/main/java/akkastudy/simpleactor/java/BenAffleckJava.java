1ea99c3a70a99319a77eab4d43956cec091568c3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 13:06:51 -0700

    added a conversation for actors

---
+++ src/main/java/akkastudy/simpleactor/java/BenAffleckJava.java
@@ -0,0 +1,22 @@
+package akkastudy.simpleactor.java;
+
+import akka.actor.ActorSelection;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class BenAffleckJava extends UntypedActor {
+    LoggingAdapter log =
+            Logging.getLogger(getContext().system(), this);
+
+    public void onReceive(Object message) {
+        ActorSelection selection = getContext().actorSelection("../mattDamonJava");
+        if (message instanceof String) {
+            log.info("Ben: Sending message to Matt Damon");
+            selection.tell("Hello, to you, how many fingers am I holding up?", self());
+        } else if (message instanceof Integer){
+            log.info("Ben: Matt Damon gave me {}", message);
+        }
+        unhandled(message);
+    }
+}
---
+++ src/main/java/akkastudy/simpleactor/java/MattDamonJava.java
@@ -0,0 +1,17 @@
+package akkastudy.simpleactor.java;
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class MattDamonJava extends UntypedActor {
+    LoggingAdapter log =
+            Logging.getLogger(getContext().system(), this);
+
+    public void onReceive(Object message) {
+        if (message instanceof String) {
+            log.info("Matt: Got message {}", message);
+            sender().tell(1, self());
+        } else unhandled(message);
+    }
+}
