87e2c104711074b5c5aa49b517d16d1954eeb391
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 15:36:23 -0700

    Upgraded to 2.4.1

--- src/main/scala/akkastudy/eventbus/scala/SimpleActorEventBus.scala
+++ src/main/scala/akkastudy/eventbus/scala/SimpleActorEventBus.scala
@@ -1,11 +1,13 @@
 package akkastudy.eventbus.scala
 
-import akka.event.{ActorClassification, ActorClassifier, ActorEventBus}
-import akka.actor.ActorRef
+import akka.event.japi.ManagedActorEventBus
+import akka.event._
+import akka.actor.{ActorSystem, ActorRef}
 
 /**
  * ActorEventBus is a trait that sets a Subscriber as an ActorRef, with a comparison of subscribers
  * ActorClassifier will create an ActorRef as the Classifier
+ *
  * @author Daniel Hinojosa
  * @since 4/9/14 9:40 PM
  *        url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
@@ -14,12 +16,18 @@ import akka.actor.ActorRef
  */
 
 
-class SimpleActorEventBus extends ActorEventBus with ActorClassifier with ActorClassification {
+class SimpleActorEventBus extends EventBus with LookupClassification{
   type Event = SimpleActorRefEvent
+  type Classifier = String
+  type Subscriber = ActorRef
 
   /* How many actor refs do you expect to be handled */
   override protected def mapSize: Int = 33
 
   /* Classify the events being sent and return an Actor Ref (which is the classifier type) */
-  override protected def classify(event: Event): Classifier = event.to
+  override protected def classify(event: Event) = event.message
+
+  override protected def publish(event: Event, subscriber: Subscriber): Unit = subscriber ! event.message
+
+  override protected def compareSubscribers(a: Subscriber, b: Subscriber): Int = a compareTo b
 }
--- src/test/scala/akkastudy/eventbus/scala/SimpleActorEventBusTest.scala
+++ src/test/scala/akkastudy/eventbus/scala/SimpleActorEventBusTest.scala
@@ -1,26 +1,27 @@
 package akkastudy.eventbus.scala
 
-import org.scalatest.{Matchers, FlatSpec}
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ActorSystem, Props}
+import org.scalatest.{FlatSpec, Matchers}
 
 /**
- *
- * @author Daniel Hinojosa
- * @since 4/9/14 9:36 PM
- *        url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
- *        email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
- *        tel: 505.363.5832
- */
+  *
+  * @author Daniel Hinojosa
+  * @since 4/9/14 9:36 PM
+  *        url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
+  *        email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
+  *        tel: 505.363.5832
+  */
 class SimpleActorEventBusTest extends FlatSpec with Matchers {
   behavior of "an ActorEventBus"
 
   it should """work exclusively with an ActorEventBus""" in {
-       val simpleActorEventBus = new SimpleActorEventBus()
-       val actorSystem = ActorSystem("SimpleActorSystem")
-       val simpleActorListenerRef = actorSystem.actorOf(Props[SimpleActorListener], "simpleActorListener")
-       val simpleActorRef = actorSystem.actorOf(Props[SimpleActor], "simpleActor")
 
-       simpleActorEventBus.subscribe(simpleActorListenerRef, simpleActorRef)
-       simpleActorEventBus.publish(SimpleActorRefEvent("Category-A", "Category-A message", "2012-01-12 20:11:03", simpleActorRef))
+    val actorSystem = ActorSystem("SimpleActorSystem")
+    val simpleActorEventBus = new SimpleActorEventBus()
+    val simpleActorListenerRef = actorSystem.actorOf(Props[SimpleActorListener], "simpleActorListener")
+    val simpleActorRef = actorSystem.actorOf(Props[SimpleActor], "simpleActor")
+
+    simpleActorEventBus.subscribe(simpleActorListenerRef, "Hello?")
+    simpleActorEventBus.publish(SimpleActorRefEvent("Category-A", "Category-A message", "2012-01-12 20:11:03", simpleActorRef))
   }
 }
