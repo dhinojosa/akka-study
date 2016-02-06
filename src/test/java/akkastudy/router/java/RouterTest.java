2b9ede4dd695b26c6e3ade19a3e7e9293834e935
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 14:46:51 -0700

    added content for RouterTest

--- src/test/java/akkastudy/router/java/RouterTest.java
+++ src/test/java/akkastudy/router/java/RouterTest.java
@@ -1,11 +1,50 @@
 package akkastudy.router.java;
 
-/**
- * @author Daniel Hinojosa
- * @since 2/5/16 12:52 PM
- * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
- * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
- * tel: 505.363.5832
- */
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.routing.FromConfig;
+import akkastudy.SenderActor;
+import akkastudy.simpleactor.java.SimpleActorJava;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.junit.Test;
+import scala.concurrent.Await;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
+
 public class RouterTest {
+
+    @Test
+    public void testRouterFromConfiguration() throws Exception {
+        Config config = ConfigFactory.load();
+
+        ActorSystem actorSystem = ActorSystem.create("MySystem",
+                config.getConfig("routing-system").withFallback(config));
+
+        ActorRef router = actorSystem.
+                actorOf(Props.create(SimpleActorJava.class)
+                        .withRouter(FromConfig.getInstance()), "simplerouter");
+
+        ActorRef senderActor = actorSystem.actorOf
+                (Props.create(SenderActor.class));
+
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+        router.tell("test", senderActor);
+
+        Thread.sleep(15000);
+        Await.result(actorSystem.terminate(),
+                Duration.apply(10, TimeUnit.SECONDS));
+
+    }
 }
