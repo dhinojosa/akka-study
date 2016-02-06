edf8d1835e669c6fb6f2d607b275bf94c4863309
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 15:30:57 -0700

    Added lifecycle calls to see what is really going on..

--- src/main/java/akkastudy/supervisorstrategy/java/ExceptionalChildActor.java
+++ src/main/java/akkastudy/supervisorstrategy/java/ExceptionalChildActor.java
@@ -1,8 +1,12 @@
 package akkastudy.supervisorstrategy.java;
 
 import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 public class ExceptionalChildActor extends UntypedActor {
+
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
     @Override
     public void onReceive(Object message) throws Exception {
@@ -11,5 +15,17 @@ public class ExceptionalChildActor extends UntypedActor {
         else if (message.equals("IllegalArgumentException")) throw new IllegalArgumentException("Oh no");
         else if (message.equals("Exception")) throw new Exception("Oh no");
         else sender().tell(String.format("Message Received %s", message), null);
+    }
+
+
+    @Override
+    public void postStop() throws Exception {
+        log.info("Exceptional Child Actor Stopped");
+        super.postStop();
+    }
+
+    public void postRestart(Throwable reason) throws Exception {
+        log.info("Exceptional Child Actor Restarted");
+        super.postRestart(reason);
     }
 }
