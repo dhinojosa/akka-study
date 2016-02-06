8021b217861a73a6c157e5bfc31eb92c5cf9d66d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 15:35:38 -0700

    Added log.infos to see what is going on.

--- src/main/java/akkastudy/locationtransparency/java/SenderActorJava.java
+++ src/main/java/akkastudy/locationtransparency/java/SenderActorJava.java
@@ -1,5 +1,7 @@
 package akkastudy.locationtransparency.java;
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -8,9 +10,13 @@ public class SenderActorJava extends UntypedActor {
     LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
     public void onReceive(Object message) {
-        if (message instanceof String)
-            log.info("Received String message in SimpleActorJava: {}", message);
-        else
+        if (message instanceof String) {
+            log.info("Received String message in SenderActorJava: {}", message);
+            log.info("Creating SimpleActorJava");
+            ActorRef simpleActorJava = getContext().system().actorOf(Props.create(SimpleActorJava.class), "simpleActorJava");
+            simpleActorJava.tell(message, self());
+        } else {
             unhandled(message);
+        }
     }
 }
