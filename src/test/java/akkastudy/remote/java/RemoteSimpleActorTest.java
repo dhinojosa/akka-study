bb28f2b0659429006948ed088c9a92b7a8d1453d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 15:23:36 -0700

    Simplified Remote Simple Actor Test with new shutdown

--- src/test/java/akkastudy/remote/java/RemoteSimpleActorTest.java
+++ src/test/java/akkastudy/remote/java/RemoteSimpleActorTest.java
@@ -3,36 +3,30 @@ package akkastudy.remote.java;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akkastudy.simpleactor.java.SimpleActorJava;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.junit.Test;
+import scala.concurrent.Await;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 public class RemoteSimpleActorTest {
 
     @Test
     public void testActor() throws Exception {
-        Config config = ConfigFactory.load();
+        Config config = ConfigFactory.load(); //load conf
 
-//        ActorSystem remoteSystem = ActorSystem.create("remoteActorSystem",
-//                config.getConfig("remote-system").withFallback(config));
-//        ActorSystem localSystem = ActorSystem.create("localActorSystem",
-//                config.getConfig("local-remote-binding-system").withFallback(config));
+        ActorSystem remoteSystem = ActorSystem.create("remoteSystem",
+                config.getConfig("remote-system").withFallback(config));
 
 
-        ActorSystem remoteSystem = ActorSystem.create("actorSystemBeta",
-                config.getConfig("remote-system-beta").withFallback(config));
+        ActorRef simpleActorJava = remoteSystem.actorOf
+                (Props.create(SimpleActorJava.class), "senderActorJava");
 
-        ActorSystem localSystem = ActorSystem.create("actorSystemAlpha",
-                ConfigFactory.load("remote-system-alpha"));
-
-
-        ActorRef senderActor = localSystem.actorOf(Props.create(SenderActor.class), "senderActorJava");
+        simpleActorJava.tell("Send Message", remoteSystem.deadLetters());
 
         Thread.sleep(10000);
-        localSystem.shutdown();
-        localSystem.awaitTermination();
-        remoteSystem.shutdown();
-        remoteSystem.awaitTermination();
+        Await.result(remoteSystem.terminate(), Duration.apply(10, TimeUnit.SECONDS));
     }
 }
