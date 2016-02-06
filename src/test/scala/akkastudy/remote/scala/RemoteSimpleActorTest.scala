c8cc3278bb47160e9c13e1fdde88bc96994e4db9
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-02-06 14:15:35 -0700

    Using new termination

--- src/test/scala/akkastudy/remote/scala/RemoteSimpleActorTest.scala
+++ src/test/scala/akkastudy/remote/scala/RemoteSimpleActorTest.scala
@@ -1,30 +1,34 @@
 package akkastudy.remote.scala
 
-import org.scalatest.FlatSpec
+import java.util.concurrent.TimeUnit
+
+import org.scalatest.{FunSuite, FlatSpec}
 import akka.actor.{Deploy, Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
 
-class RemoteSimpleActorTest extends FlatSpec {
-  behavior of "A simple actor"
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
-  it should "receive our message in Scala but remotely" in {
+class RemoteSimpleActorTest extends FunSuite {
+
+  test("receive our message in Scala but remotely") {
     val config = ConfigFactory.load()
-    val actorSystemBeta = ActorSystem("actorSystemBeta", config.getConfig("remote-system-beta").withFallback(config))
-    val actorSystemAlpha  = ActorSystem("actorSystemAlpha", config.getConfig("remote-system-alpha").withFallback(config))
+    val actorSystemBeta = ActorSystem("actorSystemBeta",
+      config.getConfig("remote-system-beta").withFallback(config))
+    val actorSystemAlpha  = ActorSystem("actorSystemAlpha",
+      config.getConfig("remote-system-alpha").withFallback(config))
 
     val simpleActor = actorSystemAlpha.actorOf(Props[SimpleActorScala], name = "simpleActorScala")
 
     simpleActor ! "Do it"
 
-    Thread.sleep(5000)
+    Thread.sleep(10000)
 
-    actorSystemAlpha.shutdown()
-    actorSystemAlpha.awaitTermination()
-    actorSystemBeta.shutdown()
-    actorSystemBeta.awaitTermination()
+    Await.result(actorSystemAlpha.terminate(), Duration(10, TimeUnit.SECONDS))
+    Await.result(actorSystemBeta.terminate(), Duration(10, TimeUnit.SECONDS))
   }
 
-  it should "receive our message in Scala but remotely with different setup" in {
+  test("receive our message in Scala but remotely with different setup") {
     val config = ConfigFactory.load()
     val actorSystemBeta = ActorSystem("actorSystemBeta", config.getConfig("remote-system-beta").withFallback(config))
     val actorSystemAlpha  = ActorSystem("actorSystemAlpha", config.getConfig("remote-system-alpha").withFallback(config))
@@ -34,24 +38,7 @@ class RemoteSimpleActorTest extends FlatSpec {
     senderActor ! "Do it"
 
     Thread.sleep(5000)
-    actorSystemAlpha.shutdown()
-    actorSystemAlpha.awaitTermination()
-    actorSystemBeta.shutdown()
-    actorSystemBeta.awaitTermination()
-  }
-
-  it should "be able to call a remote actor via URL" in {
-    val config = ConfigFactory.load()
-    val remoteActorSystem  = ActorSystem("remoteActorSystem", config.getConfig("remote-system").withFallback(config))
-
-    val senderActor = remoteActorSystem.actorOf(Props[SimpleActorScala], name="simpleActorScala")
-
-    senderActor ! "Do it"
-
-    remoteActorSystem.actorSelection("akka.tcp://remoteActorSystem:10190/user/simpleActorScala")
-
-    Thread.sleep(5000)
-    remoteActorSystem.shutdown()
-    remoteActorSystem.awaitTermination()
+    Await.result(actorSystemAlpha.terminate(), Duration(10, TimeUnit.SECONDS))
+    Await.result(actorSystemBeta.terminate(), Duration(10, TimeUnit.SECONDS))
   }
 }
